8f4f1ca68c832b64b458835a7a4da66b4065e017
edmund1645 <edmund.timfon@gmail.com>
2019-09-09 11:08:42 +0100

    add new replies

--- modules/replies.js
+++ modules/replies.js
@@ -1,20 +1,32 @@
 module.exports = [
   'Do I know you?',
-  'next year',
-  `You look like a sinner, let me check your name
-  ........
+  'Next year',
+  `You look like a sinner, let me check your name.
+  
+  *searches for hours*
 
-  I can't find your name, let me transfer you.
+  I can't find your name, let me transfer you to Satan.
   `,
-  'lol',
-  'lmao',
-  "I'll get back to you!",
+  'LOL',
+  'LMAO',
   'when last did you win a soul for Christ?',
-  "I'm on leave",
+  "I'm on leave, ask again later.",
   `You look like a sinner, let me check your name
   
-  ......
+  *searches for hours*
 
   Oh, here it is, let me get back to you.
+  `,
+  `L
+      0
+          L
+  `,
   `
+    L
+        M
+            A
+                O
+  `,
+  "Don't worry, I'll attend to you in 2025",
+  "I'll see what I can do."
 ];
