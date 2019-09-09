958d13536828d2d3eaf70ffcc16075d494fed214
edmund1645 <edmund.timfon@gmail.com>
2019-09-09 11:40:07 +0100

    optional replies

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ const stream = T.stream('statuses/filter', {
   track: ['God when?', 'God am i not your'],
   lang: 'en'
 });
-
+const shouldReply = () => {
+  if (Math.random() >= 0.35) {
+    return true;
+  } else {
+    return false;
+  }
+};
 stream.on('tweet', tweet => {
   let lcText = tweet.text.toLowerCase();
-  if (lcText.startsWith('god when') || lcText.endsWith('god when?') || lcText.endsWith('god when')) {
-    Twitter.reply(tweet, `${getReply(replies)}`);
-    console.log(tweet.text);
-    return;
-  } else if (lcText.includes('god am i not your')) {
-    Twitter.reply(tweet, `${getReply(replies)}`);
-    console.log(tweet.text);
-    return;
+  if (shouldReply()) {
+    if (lcText.startsWith('god when') || lcText.endsWith('god when?') || lcText.endsWith('god when')) {
+      Twitter.reply(tweet, `${getReply(replies)}`);
+      console.log(tweet.text);
+      return;
+    } else if (lcText.includes('god am i not your')) {
+      Twitter.reply(tweet, `${getReply(replies)}`);
+      console.log(tweet.text);
+      return;
+    }
   }
 });
